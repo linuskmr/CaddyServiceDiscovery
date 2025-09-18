099c68339c80b34d0f9123cf1fe213ff4c705ed1
JaKu01 <91499494+JaKu01@users.noreply.github.com>
2025-09-18 17:15:23 +0200

    Refactor service discovery logic into a scheduler for dynamic Caddy configuration updates

--- cmd/discovery/main.go
+++ cmd/discovery/main.go
@@ -1,41 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"github.com/jaku01/caddyservicediscovery/internal/caddy"
-	dockerconnector "github.com/jaku01/caddyservicediscovery/internal/docker"
-)
+import "github.com/jaku01/caddyservicediscovery/internal/scheduler"
 
 func main() {
 
-	dockerConnector := dockerconnector.NewDockerConnector()
-	caddyConnector := caddy.NewConnector("http://localhost:2019")
-
-	config, err := caddyConnector.GetCaddyConfig()
-	if err != nil {
-		panic(err)
-	}
-
-	if config == nil {
-		fmt.Println("No config found")
-		err = caddyConnector.CreateCaddyConfig()
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	containers, err := dockerConnector.GetAllContainersWithActiveLabel()
-	if err != nil {
-		panic(err)
-	}
-
-	serverMap := make(map[string]caddy.Server)
-	for _, container := range containers {
-		reverseProxyServer := caddy.NewReverseProxyServer(container.Port, container.Upstream)
-		serverMap[container.ContainerName] = reverseProxyServer
-	}
-
-	err = caddyConnector.ReplaceServers(serverMap)
+	err := scheduler.StartScheduleDiscovery("http://localhost:2019")
 	if err != nil {
 		panic(err)
 	}
---
+++ internal/scheduler/scheduler.go
@@ -0,0 +1,79 @@
+package scheduler
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/jaku01/caddyservicediscovery/internal/caddy"
+	dockerconnector "github.com/jaku01/caddyservicediscovery/internal/docker"
+	"log"
+	"time"
+)
+
+func StartScheduleDiscovery(caddyAdminUrl string) error {
+	dockerConnector := dockerconnector.NewDockerConnector()
+	caddyConnector := caddy.NewConnector(caddyAdminUrl)
+
+	log.Println("Starting scheduler for service discovery")
+	log.Printf("Caddy admin url: %s", caddyAdminUrl)
+
+	err := createCaddyConfigIfMissing(caddyConnector)
+	if err != nil {
+		return err
+	}
+
+	lastMapState := make([]byte, 0)
+
+	for {
+		serverMap, err := getServerMap(err, dockerConnector)
+		if err != nil {
+			return err
+		}
+
+		currentState, _ := json.Marshal(serverMap)
+
+		if bytes.Equal(currentState, lastMapState) {
+			log.Println("No changes detected, skip updating")
+		} else {
+			log.Println("Server map changes detected, updating caddy configuration")
+			lastMapState = currentState
+
+			err = caddyConnector.ReplaceServers(serverMap)
+			if err != nil {
+				return err
+			}
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func getServerMap(err error, dockerConnector *dockerconnector.DockerConnector) (map[string]caddy.Server, error) {
+	containers, err := dockerConnector.GetAllContainersWithActiveLabel()
+	if err != nil {
+		return nil, err
+	}
+
+	serverMap := make(map[string]caddy.Server)
+	for _, container := range containers {
+		reverseProxyServer := caddy.NewReverseProxyServer(container.Port, container.Upstream)
+		serverMap[container.ContainerName] = reverseProxyServer
+	}
+	return serverMap, nil
+}
+
+func createCaddyConfigIfMissing(caddyConnector *caddy.Connector) error {
+	config, err := caddyConnector.GetCaddyConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if config == nil {
+		fmt.Println("No config found")
+		err = caddyConnector.CreateCaddyConfig()
+		if err != nil {
+			panic(err)
+		}
+	}
+	return err
+}
